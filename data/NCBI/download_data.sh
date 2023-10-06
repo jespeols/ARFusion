b8790e4683d224db0b51d774d899a35120220b1d
jespeols <jesperolsson9@hotmail.com>
2023-10-06 11:22:38 +0200

    Initial commit: Add NCBI download script

---
+++ data/NCBI/download_data.sh
@@ -0,0 +1,3 @@
+echo "Downloading data from NCBI"
+wget -O AMR_Ecoli_Shigella_20231005.tsv \
+https://ftp.ncbi.nlm.nih.gov/pathogen/Results/Escherichia_coli_Shigella/PDG000000004.4059/Metadata/PDG000000004.4059.metadata.tsv
