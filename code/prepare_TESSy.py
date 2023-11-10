4d5a96ed1289bdb96ab0fb3f521529f31eb5a9ed
jespeols <jesperolsson9@hotmail.com>
2023-11-10 10:55:57 +0100

    update dataset naming

--- code/prepare_TESSy.py
+++ code/prepare_TESSy.py
@@ -8,16 +8,13 @@ from data_preprocessing import preprocess_TESSy
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 if __name__ == '__main__':
-    path = 'data/raw/TESSy.csv'
-    save_path = 'data/TESSy_parsed.pkl'
-    
     config_path = BASE_DIR / "config_pheno.yaml"
     with open(config_path, "r") as config_file:
         config = yaml.safe_load(config_file)
     config = config['data']
-    _ = preprocess_TESSy(path=path,
+    _ = preprocess_TESSy(path=config['path'],
                         pathogens=config['pathogens'],
-                        save_path=save_path,
+                        save_path=config['save_path'],
                         except_antibiotics=config['except_antibiotics'],
                         impute_age=config['impute_age'],
                         impute_gender=config['impute_gender'])
