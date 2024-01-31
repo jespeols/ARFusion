99bc849905b8edc15a31783669c7e969891f20ea
jespeols <jesperolsson9@hotmail.com>
2024-01-31 09:37:58 +0100

    generate results for uniform class masking

--- multimodal/datasets.py
+++ multimodal/datasets.py
@@ -427,13 +427,13 @@ class MMFinetuneDataset(Dataset):
             for i, pheno_seq in enumerate(pheno_sequences):
                 # self.rng.shuffle(pheno_seq) # if positional encoding is used, sequences ought to be shuffled
                 classes = ab_classes[i]                # randomly choose one class to keep
-                # keep_class = self.rng.choice(np.unique(classes)) # all classes are equally likely
-                # keep_class = self.rng.choice(classes) # more frequent classes are more likely
                 unique_classes, counts = np.unique(classes, return_counts=True)
                 freq = counts / counts.sum()
                 inv_freq = 1 / freq
                 prob = inv_freq / inv_freq.sum()
                 keep_class = self.rng.choice(unique_classes, p=prob) # less frequent classes are more likely
+                # keep_class = self.rng.choice(unique_classes) # all classes are equally likely
+                # keep_class = self.rng.choice(classes) # more frequent classes are more likely
                 kept_classes.append(keep_class)
                 seq_len = len(pheno_seq)
                 target_res = [-1]*self.num_ab
